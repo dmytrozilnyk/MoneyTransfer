ec4959db417d25702053c3e1be300a936e3e901e
Dmytro Zilnyk <>
2018-03-03 20:06:03 +0100

    Assets

---
+++ src/app/app.firebaseconfig.ts
@@ -0,0 +1,8 @@
+export var config = {
+    apiKey: "AIzaSyAvYTZkxBiEjf1_4K7r9OZ_2fDn0LkF0lg",
+    authDomain: "moneytransfer-54ef9.firebaseapp.com",
+    databaseURL: "https://moneytransfer-54ef9.firebaseio.com",
+    projectId: "moneytransfer-54ef9",
+    storageBucket: "moneytransfer-54ef9.appspot.com",
+    messagingSenderId: "1089530657322"
+  };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,97 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { TabsPage } from '../pages/tabs/tabs';
+import { RegisterPage } from '../pages/register/register';
+import { CreateAccountPage } from '../pages/register/create-account/create-account';
+import { SetpasswordPage } from '../pages/setpassword/setpassword';
+import { CardPage } from '../pages/tabs/card/card';
+import { FriendsPage } from '../pages/tabs/friends/friends';
+import { HomePage } from '../pages/tabs/home/home';
+import { ProfilePage } from '../pages/tabs/profile/profile';
+import { TransferPage } from '../pages/tabs/transfer/transfer';
+import { ProfileFotoPage } from '../pages/register/profile-foto/profile-foto'
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import { ApiClientService } from '../client/index';
+import { config } from './app.firebaseconfig';
+
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader } from '@ngx-translate/core';
+
+import { IonicStorageModule } from '@ionic/storage';
+
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+import { Camera } from '@ionic-native/camera';
+import { ImagePicker } from '@ionic-native/image-picker';
+import { Base64 } from '@ionic-native/base64';
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LoginPage,
+    TabsPage,
+    RegisterPage,
+    CreateAccountPage,
+    SetpasswordPage,
+    CardPage,
+    FriendsPage,
+    HomePage,
+    ProfilePage,
+    TransferPage,
+    ProfileFotoPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp,{
+      scrollAssist: false,
+      autoFocusAssist: false
+      }),
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      }),
+      AngularFireModule.initializeApp(config),
+      HttpClientModule,
+      IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage,
+    TabsPage,
+    RegisterPage,
+    CreateAccountPage,
+    SetpasswordPage,
+    CardPage,
+    FriendsPage,
+    HomePage,
+    ProfilePage,
+    TransferPage,
+    ProfileFotoPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuth,
+    ApiClientService,
+    FingerprintAIO,
+    Camera,
+    ImagePicker,
+    Base64 
   ]
 })
 export class AppModule {}
